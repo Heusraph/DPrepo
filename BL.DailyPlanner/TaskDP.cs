2a72208a5f91ba43aedc9beca908b69f64f92b9e
Heusraph <raphaelbautista2@gmail.com>
2025-03-29 00:56:44 +0800

    new

---
+++ BL.DailyPlanner/TaskDP.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.DailyPlanner
+{
+    public class DPTask
+    {
+        public static List<string> tasks = new List<string>();
+        public static List<string> times = new List<string>();
+
+        public static void AddTask(string task, string time)
+        {
+
+
+            DPTask.tasks.Add(task);
+            DPTask.times.Add(time);
+
+
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using BL.DailyPlanner;
+
+
+namespace DailyPlanner2
+{
+    internal class Program
+    {
+
+
+        static void Main()
+        {
+            while (true)
+            {
+                Console.WriteLine("Welcome to Daily Planner! Please select an action:");
+                Console.WriteLine("[1] Add Task");
+                Console.WriteLine("[2] View Tasks");
+                Console.WriteLine("[3] Exit");
+
+                Console.Write("Enter choice: ");
+                int choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.Write("What will be your task?: ");
+                        string task = Console.ReadLine();
+
+                        Console.Write("Enter the time for your task (morning/noon/evening): ");
+                        string time = Console.ReadLine();
+                        DPTask.AddTask(task, time);
+                        Console.WriteLine("Task added successfully!");
+                        break;
+                    case 2:
+                        ViewTasks();
+                        break;
+                    case 3:
+                        Console.WriteLine("Goodbye! Have a productive day.");
+                        return;
+                    default:
+                        Console.WriteLine("Invalid choice. Try again.");
+                        break;
+                }
+            }
+        }
+
+
+        static void ViewTasks()
+        {
+            if (DPTask.tasks.Count == 0)
+            {
+                Console.WriteLine("No tasks available.");
+                return;
+            }
+
+            Console.WriteLine("Your Tasks:");
+            for (int i = 0; i < DPTask.tasks.Count; i++)
+            {
+                Console.WriteLine($"[{i + 1}] {DPTask.tasks[i]} at {DPTask.times[i]}");
+            }
+        }
+    }
+}
